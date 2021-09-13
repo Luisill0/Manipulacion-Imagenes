ca2c97ac28b3bffb63289b0a17f4e742508fbd98
Luis Naranjo <a288074@alumnos.uaslp.mx>
2021-09-13 16:38:13 -0500

    First commit - Image Filters

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace Manipulacion_Imagenes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonGrayScale = new System.Windows.Forms.Button();
+            this.buttonBW = new System.Windows.Forms.Button();
+            this.buttonRestore = new System.Windows.Forms.Button();
+            this.buttonSepia = new System.Windows.Forms.Button();
+            this.buttonNegative = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(125, 12);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 0;
+            this.buttonOpen.Text = "Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonGrayScale
+            // 
+            this.buttonGrayScale.Location = new System.Drawing.Point(206, 12);
+            this.buttonGrayScale.Name = "buttonGrayScale";
+            this.buttonGrayScale.Size = new System.Drawing.Size(75, 23);
+            this.buttonGrayScale.TabIndex = 0;
+            this.buttonGrayScale.Text = "GrayScale";
+            this.buttonGrayScale.UseVisualStyleBackColor = true;
+            this.buttonGrayScale.Click += new System.EventHandler(this.buttonGrayScale_Click);
+            // 
+            // buttonBW
+            // 
+            this.buttonBW.Location = new System.Drawing.Point(287, 12);
+            this.buttonBW.Name = "buttonBW";
+            this.buttonBW.Size = new System.Drawing.Size(75, 23);
+            this.buttonBW.TabIndex = 0;
+            this.buttonBW.Text = "Black/White";
+            this.buttonBW.UseVisualStyleBackColor = true;
+            this.buttonBW.Click += new System.EventHandler(this.buttonBW_Click);
+            // 
+            // buttonRestore
+            // 
+            this.buttonRestore.Location = new System.Drawing.Point(530, 12);
+            this.buttonRestore.Name = "buttonRestore";
+            this.buttonRestore.Size = new System.Drawing.Size(75, 23);
+            this.buttonRestore.TabIndex = 0;
+            this.buttonRestore.Text = "Restore";
+            this.buttonRestore.UseVisualStyleBackColor = true;
+            this.buttonRestore.Click += new System.EventHandler(this.buttonRestore_Click);
+            // 
+            // buttonSepia
+            // 
+            this.buttonSepia.Location = new System.Drawing.Point(368, 12);
+            this.buttonSepia.Name = "buttonSepia";
+            this.buttonSepia.Size = new System.Drawing.Size(75, 23);
+            this.buttonSepia.TabIndex = 0;
+            this.buttonSepia.Text = "Sepia";
+            this.buttonSepia.UseVisualStyleBackColor = true;
+            this.buttonSepia.Click += new System.EventHandler(this.buttonSepia_Click);
+            // 
+            // buttonNegative
+            // 
+            this.buttonNegative.Location = new System.Drawing.Point(449, 12);
+            this.buttonNegative.Name = "buttonNegative";
+            this.buttonNegative.Size = new System.Drawing.Size(75, 23);
+            this.buttonNegative.TabIndex = 0;
+            this.buttonNegative.Text = "Negative";
+            this.buttonNegative.UseVisualStyleBackColor = true;
+            this.buttonNegative.Click += new System.EventHandler(this.buttonNegative_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.buttonRestore);
+            this.Controls.Add(this.buttonNegative);
+            this.Controls.Add(this.buttonSepia);
+            this.Controls.Add(this.buttonBW);
+            this.Controls.Add(this.buttonGrayScale);
+            this.Controls.Add(this.buttonOpen);
+            this.Name = "Form1";
+            this.Text = "Image Editor";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Button buttonGrayScale;
+        private System.Windows.Forms.Button buttonBW;
+        private System.Windows.Forms.Button buttonRestore;
+        private System.Windows.Forms.Button buttonSepia;
+        private System.Windows.Forms.Button buttonNegative;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Manipulacion_Imagenes
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap bmpOG;
+        private Bitmap bmpNew;
+        public Form1()
+        {
+            InitializeComponent();      
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            try
+            {
+                if (bmpNew != null)
+                {
+                    e.Graphics.DrawImage(bmpNew, 400 - bmpNew.Width / 2, 300 - bmpNew.Height / 2);
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+
+            DialogResult res = dlg.ShowDialog();
+
+            if (res == DialogResult.OK)
+            {
+                bmpOG = (Bitmap) Image.FromFile(dlg.FileName);
+                bmpNew = (Bitmap) bmpOG.Clone();
+            }
+
+            Invalidate();
+        }
+
+        private void buttonGrayScale_Click(object sender, EventArgs e)
+        {
+            Color pColor;
+            int grayScale;
+
+            for(int x = 0; x < bmpNew.Width; x++)
+            {
+                for(int y = 0; y < bmpNew.Height; y++)
+                {
+                    pColor = bmpNew.GetPixel(x, y);
+                    grayScale = (int) Math.Round(pColor.R * 0.299 + pColor.G * 0.587 + pColor.B * 0.114);
+                    bmpNew.SetPixel(x, y, Color.FromArgb(pColor.A,grayScale,grayScale,grayScale));
+                }
+            }
+
+            Invalidate();
+        }
+
+        private void buttonBW_Click(object sender, EventArgs e)
+        {
+            Color pColor;
+            int colorValue;
+            
+            for (int x = 0; x < bmpNew.Width; x++)
+            {
+                for (int y = 0; y < bmpNew.Height; y++)
+                {
+                    pColor = bmpNew.GetPixel(x, y);
+                    colorValue = ((pColor.R + pColor.G + pColor.B) / 3) > 128 ? 255 : 0;
+
+                    bmpNew.SetPixel(x, y, Color.FromArgb(pColor.A, colorValue, colorValue, colorValue));
+                }
+            }
+
+            Invalidate();
+        }
+
+        private void buttonSepia_Click(object sender, EventArgs e)
+        {
+            Color pColor;
+            int tr, tg, tb;
+
+            for (int x = 0; x < bmpNew.Width; x++)
+            {
+                for (int y = 0; y < bmpNew.Height; y++)
+                {
+                    pColor = bmpNew.GetPixel(x, y);
+
+                    tr = (int)Math.Round((0.393 * pColor.R + 0.769 * pColor.G + 0.189 * pColor.B));
+                    tg = (int)Math.Round((0.349 * pColor.R + 0.686 * pColor.G + 0.168 * pColor.B));
+                    tb = (int)Math.Round((0.272 * pColor.R + 0.534 * pColor.G + 0.131 * pColor.B));
+
+                    tr = tr > 255 ? 255 : tr;
+                    tg = tg > 255 ? 255 : tg;
+                    tb = tb > 255 ? 255 : tb;
+
+                    bmpNew.SetPixel(x, y, Color.FromArgb(pColor.A, tr, tg, tb));
+                }
+            }
+
+            Invalidate();
+        }
+
+        private void buttonNegative_Click(object sender, EventArgs e)
+        {
+            Color pColor;
+            int nr, ng, nb;
+
+            for (int x = 0; x < bmpNew.Width; x++)
+            {
+                for (int y = 0; y < bmpNew.Height; y++)
+                {
+                    pColor = bmpNew.GetPixel(x, y);
+                    
+                    nr = 255 - pColor.R;
+                    ng = 255 - pColor.G;
+                    nb = 255 - pColor.B;
+
+                    bmpNew.SetPixel(x, y, Color.FromArgb(pColor.A,nr,ng,nb));
+                }
+            }
+
+            Invalidate();
+        }
+
+        private void buttonRestore_Click(object sender, EventArgs e)
+        {
+            bmpNew = (Bitmap)bmpOG.Clone();
+            Invalidate();
+        }
+    }
+}
